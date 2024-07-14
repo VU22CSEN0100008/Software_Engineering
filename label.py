10f555fae83327b9b9f2a5b9f275b9c68036147d
VU22CSEN0100008 <126643955+VU22CSEN0100008@users.noreply.github.com>
2024-07-14 22:05:33 +0530

    Add files via upload

---
+++ label.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4]
+y = [1, 5, 6, 8]
+plt.plot(x,y)
+plt.xlable('x-axis')
+plt.ylable('y-axis')
+plt.title('line plot')
+plt.show()
---
+++ piechart.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
+y = np.array([10,20,30,40])
+plt.pie(y)
+plt.show()
